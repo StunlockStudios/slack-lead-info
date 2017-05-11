d9aefa25f16802a268d2fd2aec9b0ab187781e01
Karl BergstrÃm <karl.anton.bergstrom@gmail.com>
2017-05-11 17:31:14 +0800

    Fix error handling and add guilds

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"strings"
 
 	"github.com/StunlockStudios/confluencegoclient"
+	"github.com/pkg/errors"
 
 	"net/http"
 
@@ -33,14 +33,29 @@ type leadInfo struct {
 	Grunts []slackUser
 }
 
+type slackError struct {
+	Message string
+}
+
 type slackInfo struct {
 	FeatureTeams []featureTeam
+	Guilds       []featureTeam
 	Leads        []leadInfo
+	Errors       []slackError
+}
+
+func (info *slackInfo) errMsg(msg string) {
+	info.Errors = append(info.Errors, slackError{Message: msg})
+}
+
+func (info *slackInfo) error(msg string, err error) {
+	wrapped := errors.Wrap(err, msg)
+	info.Errors = append(info.Errors, slackError{Message: wrapped.Error()})
 }
 
 func getUser(userList []slack.User, name string) *slack.User {
 	for _, user := range userList {
-		if user.Name == name || user.ID == name {
+		if strings.ToLower(user.Name) == strings.ToLower(name) || user.ID == name {
 			return &user
 		}
 	}
@@ -59,20 +74,30 @@ func main() {
 	http.ListenAndServe(os.Args[1], nil)
 }
 func handler(w http.ResponseWriter, r *http.Request) {
+	encoder := json.NewEncoder(w)
+	if r.URL.Query().Get("pretty") != "" {
+		encoder.SetIndent("", "    ")
+	}
+	encoder.Encode(getSlackInfo())
+}
+func getSlackInfo() (slackInfo slackInfo) {
 	confluenceAuth := confluence.BasicAuth(os.Args[4], os.Args[5])
 	confluenceAPI, err := confluence.NewWiki(os.Args[3], confluenceAuth)
 	if err != nil {
-		log.Fatal(err)
+		slackInfo.error("Failed to auth with confluence", err)
+		return
 	}
 	expand := []string{"body.storage"}
 	content, err := confluenceAPI.GetContent("12159063", expand)
 	if err != nil {
-		log.Fatal(err)
+		slackInfo.error("Failed to get confluence lead assignment page", err)
+		return
 	}
 
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(content.Body.Storage.Value))
 	if err != nil {
-		log.Fatal(err)
+		slackInfo.error("Failed to create a goquery document from the confluence content: "+content.Body.Storage.Value, err)
+		return
 	}
 	selection := doc.Find("tr")
 
@@ -116,26 +141,29 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	channels, err := api.GetChannels(true)
 	if err != nil {
-		log.Fatal(err)
+		slackInfo.error("Failed to get slack channels", err)
+		return
 	}
 
 	userList, err := api.GetUsers()
 	if err != nil {
-		log.Fatal(err)
+		slackInfo.error("Failed to get slack users", err)
+		return
 	}
 
-	var slackInfo slackInfo
 	for lead, grunts := range leadAssignments {
 		var leadUser = getUser(userList, lead)
 		if leadUser == nil {
-			log.Fatal(fmt.Sprintf("User %s not found", lead))
+			slackInfo.errMsg(fmt.Sprintf("Failed to find lead %s in the slack user list", lead))
+			continue
 		}
 		var leadInfo leadInfo
 		leadInfo.Lead = slackUserToAPI(*leadUser)
 		for _, grunt := range grunts {
 			var gruntUser = getUser(userList, grunt)
 			if gruntUser == nil {
-				log.Fatal(fmt.Sprintf("User %s not found", grunt))
+				slackInfo.errMsg(fmt.Sprintf("Failed to find grunt %s in the slack user list for lead %s", grunt, lead))
+				continue
 			}
 			leadInfo.Grunts = append(leadInfo.Grunts, slackUserToAPI(*gruntUser))
 		}
@@ -143,7 +171,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	for _, channel := range channels {
-		if strings.HasPrefix(channel.Name, "f-") {
+		if strings.HasPrefix(channel.Name, "f-") || strings.HasPrefix(channel.Name, "g-") {
 			var team featureTeam
 			team.Name = channel.Name
 			tokens := strings.Split(channel.Topic.Value, " ")
@@ -151,7 +179,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 				if strings.HasPrefix(token, "@") {
 					user := getUser(userList, token[1:])
 					if user == nil {
-						log.Fatal(fmt.Sprintf("Could not find user %s", token))
+						slackInfo.errMsg(fmt.Sprintf("Failed to find a user called %s from the channel '%s' with topic '%s'", token[1:], channel.Name, channel.Topic.Value))
 					}
 					team.Owner = new(slackUser)
 					*team.Owner = slackUserToAPI(*user)
@@ -160,12 +188,18 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			for _, member := range channel.Members {
 				user := getUser(userList, member)
 				if user == nil {
-					log.Fatal(fmt.Sprintf("Could not find user %s", member))
+					slackInfo.errMsg(fmt.Sprintf("Failed to find a matching user in the slack user list for user %s in channel %s", member, channel.Name))
 				}
 				team.Members = append(team.Members, slackUserToAPI(*user))
 			}
-			slackInfo.FeatureTeams = append(slackInfo.FeatureTeams, team)
+			if strings.HasPrefix(channel.Name, "f-") {
+				slackInfo.FeatureTeams = append(slackInfo.FeatureTeams, team)
+			} else if strings.HasPrefix(channel.Name, "g-") {
+				slackInfo.Guilds = append(slackInfo.Guilds, team)
+			} else {
+				slackInfo.errMsg(fmt.Sprintf("Failure in handling prefix of channel %s", channel.Name))
+			}
 		}
 	}
-	json.NewEncoder(w).Encode(slackInfo)
+	return slackInfo
 }
